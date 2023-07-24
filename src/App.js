7d6cd926611e35d8d7b9a5683367e932291fd4e8
Kishkin Vitalii <dimpla30@gmail.com>
2023-07-24 21:44:57 +0300

    made a conclusion of completed tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCompletedTodos } from "./utils/api"; // Укажите правильный путь к файлу api.js
+import { setCompletedTodos } from "./features/todo/todoListSlice";
 import TodoList from "./components/TodoList";
 import Progress from "./components/Progress";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const completedTodos = await fetchCompletedTodos();
+        dispatch(setCompletedTodos(completedTodos));
+      } catch (error) {
+        console.error("Ошибка при загрузке выполненных тудушек:", error);
+      }
+    };
+    loadData();
+  }, [dispatch]);
+
   return (
     <Router>
       <div>
--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
 // import { useDispatch } from "react-redux";
 
 const Progress = () => {
+  const completedTodos = useSelector(state => state.todo.todos.filter(todo => todo.completed));
+
   return (
     <div>
       <h1 className="title">List of completed tasks</h1>
       <div>
         <ul>
-          <li className="item-complete">
-         
-          </li>
+          {completedTodos.map(todo => (
+            <TodoItem key={todo.id} todo={todo} />
+          ))}
         </ul>
       </div>
     </div>
