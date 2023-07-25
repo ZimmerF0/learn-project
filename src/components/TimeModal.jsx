904b9c51237b3779e440a4dd57d827cad37d8a1d
Kishkin Vitalii <dimpla30@gmail.com>
2023-07-25 09:48:56 +0300

    checking the time in the modal window

--- src/components/TimeModal.jsx
+++ src/components/TimeModal.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
-import '../styles/TimeModal.css';
+import React, { useState } from "react";
+import "../styles/TimeModal.css";
 
 const TimeModal = ({ isOpen, onClose, onSave }) => {
-  const [time, setTime] = useState('');
+  const [time, setTime] = useState("");
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     setTime(event.target.value);
   };
 
   const handleSave = () => {
-    onSave(time);
-    onClose();
+    if (time > 0) {
+      onSave(time);
+      onClose();
+    }
   };
 
   return (
-    <div className={`modal ${isOpen ? 'open' : ''}`}>
+    <div className={`modal ${isOpen ? "open" : ""}`}>
       <div className="modal-content">
-      <span className="close" onClick={onClose}></span>
-        <h3 className='modal-title'>Enter the execution time</h3>
+        <span className="close" onClick={onClose}></span>
+        <h3 className="modal-title">Enter the execution time</h3>
         <input
           type="number"
           value={time}
           onChange={handleChange}
           placeholder="Time (in minutes)"
         />
-        <button className='modal-btn' onClick={handleSave}>Save</button>
+        <button className="modal-btn" onClick={handleSave}>
+          Save
+        </button>
       </div>
     </div>
   );
